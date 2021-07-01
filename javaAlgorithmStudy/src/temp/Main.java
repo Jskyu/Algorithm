f8ad9867c3207ef38cf78eccb0d6ade17e5dfd83
o3ojsk <hisunggyu@gmail.com>
2021-07-02 02:28:44 +0900

    .

---
+++ javaAlgorithmStudy/src/temp/Main.java
@@ -0,0 +1,10 @@
+/*
+public class Main{
+    public static void main(String[] args) {
+        int x = 1, y = 6;
+        while(y--){
+            x++;
+        }
+        System.out.println(x, y);
+    }
+}*/
